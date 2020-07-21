8261b82f04930033aa3b7a437081b7a22ee361dd
Sevket Yalcin <sevket.yalcin@outlook.com>
2020-07-21 10:36:45 +0200

    Create trie.js

---
+++ trie.js
@@ -0,0 +1,15 @@
+var Trie = function(words) {
+  this.root = {};
+  
+  for(let p = 0; p < words.length; p++) {
+    this.insert(words[p]);
+  }
+};
+
+Trie.prototype.insert = function(word) {
+  let current = this.root;
+  word.split('').map(letter => {
+    current = current[letter] = current[letter] || {}
+  })
+  current.isWord = true;
+};
