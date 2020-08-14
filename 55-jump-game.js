7183c7e6a6f8dfc4a79fb867d20de88e514ebaff
SevketYALCIN <sevket.yalcin@outlook.com>
2020-08-14 18:46:26 +0200

    reorganize repo + add daily LC

---
+++ 55-jump-game.js
@@ -0,0 +1,16 @@
+var canJump = function (nums) {
+  let dp = new Array(nums.length).fill(false)
+  dp[nums.length - 1] = true
+
+  for (let i = nums.length - 2; i >= 0; i--) {
+    for (let j = 1; j <= nums[i]; j++) {
+      if (dp[i + j]) {
+        dp[i] = true
+        break;
+      }
+    }
+  }
+
+  console.log(dp)
+  return dp[0]
+};
