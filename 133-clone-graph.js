fc86970484e0505757cad0a72e74b2ef012cc136
Sevket Yalcin <sevket.yalcin@outlook.com>
2020-08-15 15:08:23 +0200

    Create 133-clone-graph.js

---
+++ 133-clone-graph.js
@@ -0,0 +1,17 @@
+var cloneGraph = function(node) {
+  // DFS
+  if (node === null) {
+    return null;
+  }
+  
+  const map = new Map();
+  const clone = root => {
+    if (!map.has(root.val)) {
+      map.set(root.val, new Node(root.val));
+      map.get(root.val).neighbors = root.neighbors.map(clone);
+    }
+    return map.get(root.val);
+  }
+  
+  return clone(node);
+};
